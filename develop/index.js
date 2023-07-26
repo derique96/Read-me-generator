0b19ccb72b1e41cbb281f2c56f84d00edfa08e7f
derique96 <fowlkes.derique@gmail.com>
2023-07-26 18:31:57 -0400

    fixed prompts

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,57 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const readMe = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a brief description of the project',
+    },    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation you would like for your project',
+    },    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub account username',
+    },    {
+        type: 'input',
+        name: 'URL',
+        message: 'Copy and Paste the url to your deployed application',
+    },    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address',
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
+
+// Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    const filledTemplate = readMe(data);
+
+    const fileName = `example/README.md`;
+    writeToFile(fileName, filledTemplate, (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
+  });
+}
 
 // Function call to initialize app
 init();
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case "Mozilla":
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    case "Apache":
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    case "GNU GPLv3":
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    case "No license":
+      return ``;
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
